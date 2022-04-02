e623c5ab82dd473000d2c33d7d1675d111bbb844
Shahzaib Ibrahim <shahzaib.ibrahim@forwardyou.com>
2022-04-02 20:08:56 +0500

    Route and Redirect

--- src/App.js
+++ src/App.js
@@ -1,18 +1,30 @@
-import {Route} from 'react-router-dom';
-import Welcome from './pages/Welcome';
-import Products from './pages/Products';
-import MainHeader from './components/MainHeader';
+import { Route, Switch } from "react-router-dom";
+import Welcome from "./pages/Welcome";
+import ProductDetail from "./pages/ProductDetail";
+import Products from "./pages/Products";
+import MainHeader from "./components/MainHeader";
+import { Redirect } from "react-router-dom";
 
 function App() {
   return (
     <div>
-      <MainHeader / >
-      <Route path="/welcome">
-        <Welcome />
-      </Route>
-      <Route path="/products">
-        <Products />
-      </Route>
+      <MainHeader />
+      <main>
+        <Switch>
+          <Route path='/' exact >
+            <Redirect to='/welcome'/>
+          </Route>
+          <Route path="/welcome">
+            <Welcome />
+          </Route>
+          <Route path="/products" exact>
+            <Products />
+          </Route>
+          <Route path="/products/:productId">
+            <ProductDetail />
+          </Route>
+        </Switch>
+      </main>
     </div>
   );
 }
